633a8ddc776796361f78fa1b58bb9560ce34736f
annuragsingh2017 <annuragsingh2017@gmail.com>
2024-03-05 21:06:15 +0530

    refactoring the code

---
+++ src/component/Form.jsx
@@ -0,0 +1,121 @@
+import { useEffect } from 'react';
+
+const Form = ({
+	formData: {
+		setTask,
+		setTaskError,
+		task,
+		taskError,
+		setDescription,
+		setDescriptionError,
+		description,
+		descriptionError,
+		dropDown,
+		setDropDown,
+		editableMode,
+		setTodos,
+		todos,
+		editedData,
+		setEditableMode,
+		setEditedData,
+		setSelectAll,
+	},
+}) => {
+	const handleUpdate = () => {
+		if (editableMode) {
+			setTodos(
+				todos.map((data) => {
+					if (data.id === editedData.id) {
+						return {
+							...data,
+							task,
+							description,
+							dropDown,
+						};
+					}
+					return data;
+				})
+			);
+		}
+		setEditableMode(false);
+		setEditedData({});
+		setTask('');
+		setDescription('');
+		setDropDown('high');
+	};
+	const handleRemoveCompleted = () => {
+		setSelectAll(false);
+		setTodos(todos?.filter((item) => !item.isSelected));
+	};
+
+	useEffect(() => {
+		if (todos?.length > 0 && todos?.every((val) => val.isSelected === true)) {
+			setSelectAll(true);
+		}
+	}, [setSelectAll, todos]);
+
+	const handleSubmit = () => {
+		if (task.trim() === '' && description.trim() === '') {
+			setDescriptionError('Task and Description cannot be empty');
+			return;
+		}
+		if (task.trim() === '') {
+			setTaskError('Task cannot be empty');
+			return;
+		}
+		if (description.trim() === '') {
+			setDescriptionError('Description cannot be empty');
+			return;
+		}
+		setTaskError('');
+		setDescriptionError('');
+		setTodos([...todos, { task, description, isSelected: false, dropDown, id: Date.now() }]);
+		setTask('');
+		setDescription('');
+		setDropDown('high');
+	};
+	return (
+		<>
+			<h2>TODO</h2>
+			<input
+				className="input"
+				placeholder="Add Task"
+				onChange={(e) => {
+					setTask(e.target.value);
+					setTaskError('');
+				}}
+				value={task}
+			/>
+			{taskError && <p className="error">{taskError}</p>}
+			<input
+				className="input"
+				placeholder="Add Task Description"
+				onChange={(e) => {
+					setDescription(e.target.value);
+					setDescriptionError('');
+				}}
+				value={description}
+			/>
+			{descriptionError && <p className="error">{descriptionError}</p>}
+
+			<label>Priority</label>
+			<select
+				value={dropDown}
+				onChange={(e) => {
+					setDropDown(e.target.value);
+				}}
+			>
+				<option value="high">high</option>
+				<option value="medium"> medium</option>
+				<option value="low"> low</option>
+			</select>
+			<div className="buttons">
+				<button onClick={editableMode ? handleUpdate : handleSubmit}>
+					{editableMode ? 'update' : 'Add'} Task
+				</button>
+				<button onClick={() => handleRemoveCompleted()}> Remove Completed</button>
+			</div>
+		</>
+	);
+};
+export default Form;
---
+++ src/component/Todo.jsx
@@ -0,0 +1,111 @@
+import { useState } from 'react';
+import './todo.css';
+import { renderPagination } from './pagination';
+import RenderTodo from './pages';
+import Form from './Form';
+
+const Todo = () => {
+	const [todos, setTodos] = useState([]);
+	const [task, setTask] = useState('');
+	const [description, setDescription] = useState('');
+	const [dropDown, setDropDown] = useState('high');
+	const [editableMode, setEditableMode] = useState(false);
+	const [editedData, setEditedData] = useState({});
+	const [selectAll, setSelectAll] = useState(false);
+	const [taskError, setTaskError] = useState('');
+	const [descriptionError, setDescriptionError] = useState('');
+	const [currentPage, setCurrentPage] = useState(1);
+	let itemsPerPage = 4;
+	const totalPages = Math.ceil(todos?.length / itemsPerPage);
+	const startIndex = (currentPage - 1) * itemsPerPage;
+	const endIndex = startIndex + itemsPerPage;
+
+	const handleDelete = (id) => {
+		setTodos(todos?.filter((todo) => todo.id !== id));
+	};
+	const handleEdit = (todo) => {
+		setEditableMode(!editableMode);
+		setEditedData(todo);
+		setTask(todo.task);
+		setDescription(todo.description);
+		setDropDown(todo.dropDown);
+	};
+	const handleCheckbox = (todo, e) => {
+		setSelectAll(false);
+		setTodos(
+			todos?.map((data) => {
+				if (data.id === todo.id) {
+					return {
+						...data,
+						isSelected: e.target.checked,
+					};
+				}
+				return data;
+			})
+		);
+	};
+	const handleAllChecked = (e) => {
+		setSelectAll(!selectAll);
+		setTodos(
+			todos.map((data) => ({
+				...data,
+				isSelected: e.target.checked,
+			}))
+		);
+	};
+	const handleClick = (page) => {
+		setCurrentPage(page);
+	};
+
+	return (
+		<div className="todo">
+			<Form
+				formData={{
+					setTask,
+					setTaskError,
+					task,
+					taskError,
+					setDescription,
+					setDescriptionError,
+					description,
+					descriptionError,
+					dropDown,
+					setDropDown,
+					editableMode,
+					setTodos,
+					todos,
+					editedData,
+					setEditableMode,
+					setEditedData,
+					setSelectAll,
+				}}
+			/>
+
+			<table>
+				<tr>
+					<th>
+						{' '}
+						<input type="checkbox" checked={selectAll} onChange={(e) => handleAllChecked(e)} />
+					</th>
+					<th>Task</th>
+					<th>Description</th>
+					<th>Priority</th>
+					<th>Actions</th>
+				</tr>
+
+				{todos?.slice(startIndex, endIndex)?.map((todo) => (
+					<RenderTodo
+						todo={todo}
+						handleCheckbox={handleCheckbox}
+						handleEdit={handleEdit}
+						handleDelete={handleDelete}
+					/>
+				))}
+			</table>
+			<div className="pagination buttons fixed-pagination">
+				{renderPagination(totalPages, handleClick, currentPage, setCurrentPage)}
+			</div>
+		</div>
+	);
+};
+export default Todo;
